ecb2a894719b01406b378b87ebc623c9b0b52af5
dejosemario <josemariaodumodu@gmail.com>
2024-03-27 23:47:56 +0100

    feat: create login route

--- 05_assignment/src/controllers/auth.controller.js
+++ 05_assignment/src/controllers/auth.controller.js
@@ -4,7 +4,12 @@ import * as validation from "../validations/auth.validation.js";
 export async function register(req, res) {
   try {
     const { name, email, password, confirmPassword } = req.body;
-    const user = await authService.register(name, email, password, confirmPassword);
+    const user = await authService.register(
+      name,
+      email,
+      password,
+      confirmPassword
+    );
     res
       .status(201)
       .json({ message: "User registered successfully", user: user });
@@ -13,6 +18,12 @@ export async function register(req, res) {
   }
 }
 
-export function login(req, res) {
-  res.send("Login successful");
+export async function login(req, res) {
+  try {
+    const { email, password } = req.body;
+    const token = await authService.login(email, password);
+    res.json({ message: "Login successful", accessToken: token });
+  } catch {
+    res.status(400).json({ message: error.message });
+  }
 }
--- 05_assignment/src/services/auth.service.js
+++ 05_assignment/src/services/auth.service.js
@@ -1,15 +1,33 @@
 import User from "../models/user.schema.js";
 import { ErrorWithStatus } from "../exceptions/error-with-status.exception.js";
 import bcrypt from "bcryptjs";
+import Jwt from "jsonwebtoken";
 
-export const register = async (name, email, password, confirmPassword) =>{
-    const user = await User.findOne({email});
-    if(user){
-        throw new ErrorWithStatus("User already exists", 400);
-    }
-    password = await bcrypt.hash(password, 10);
-    const newUser = new User({name, email, password, confirmPassword});
-    await newUser.save();
-    delete newUser.password;
-    return newUser;
-}
+export const register = async (name, email, password, confirmPassword) => {
+  const user = await User.findOne({ email });
+  if (user) {
+    throw new ErrorWithStatus("User already exists", 400);
+  }
+  password = await bcrypt.hash(password, 10);
+  const newUser = new User({ name, email, password, confirmPassword });
+  await newUser.save();
+  delete newUser.password;
+  return newUser;
+};
+
+export const login = async (email, password) => {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new ErrorWithStatus("User not found", 404);
+  }
+  if (!bycrypt.compareSync(password, user.password)) {
+    throw new ErrorWithStatus("Invalid password", 400);
+  }
+  const JWT_SECRET = process.env.JWT_SECRET;
+  const token = Jwt.sign(
+    { email: user.email, _id: user._id, sub: user._id },
+    JWT_SECRET,
+    { expiresIn: "10m" }
+  );
+  return token;
+};
